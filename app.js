5539d7e21f778648b9afed3f60cf080c5035c7f6
satyrwilder <leah.lasalla@satyrwilder.com>
2014-01-09 05:01:36 -0600

    Refactored app.js to use deferreds, minor cleanup of markup and changed jquery src to track latest stable min.

--- app.js
+++ app.js
@@ -1,72 +1,96 @@
-$(document).ready(function(){
-    var findWeather, showOutput, getForecast;
-    var findItem;
-    var url1, url2;
-    $("#search-id").keydown(function(event){
-        if(event.keyCode === 13) {
-            findWeather(findItem);
-        }
-});
-   
-$("#search-id").focus();
-    
-$("#button-id").click(function() {
-    findItem = $("#search-id").val();           //Gets user input.
-    $("#content").empty();
-    $("#message").empty();
-    if (findItem == "") {
-        $("#message").text(" Enter a city name and State to find weather.");        //blank input message.
-    } else {
-        $("#search-id").val("");            
-        findWeather(findItem);              //calls function to find weather.
-    }
-});
+/**
+ * Created by leah on 1/9/14.
+ */
 
-showOutput = function(out) {               //showoutput displays information received from json object from Forecast for developers.
-    var contentElem = $("#content");
-    contentElem.text(Date(out.currently.time));
-    contentElem.append("</br> </br> Temperature : " + out.currently.temperature + "F </br>");
-    contentElem.append(" </br> Humidity :  " + out.currently.humidity + "% </br>");
-    contentElem.append(" </br>Icon: " + out.currently.icon + "</br>");
-    contentElem.append(" </br>Wind Speed: " + out.currently.windSpeed + "mph </br>");
-    contentElem.append(" </br>Visibility: " + out.currently.visibility + "m </br>");
-    contentElem.append(" </br>pressure: " + out.currently.pressure + "mb");  
-    $("#message").text("Summary: " + out.daily.summary);    
-};
+var searchId = $("#search-id"),
+    buttonId = $("#button-id"),
+    content = $("#content"),
+    message = $("#message"),
+    cityname = $("#city-name"),
 
-getForecast = function(data) {
-    $("#message").empty();
-    $("#city-name").text("Current weather in " + data.results[0].address_components[0].long_name);       //Output city name.
-    console.log(data);
-    url2 = 'https://api.forecast.io/forecast/73ae89cc503818fc307b31e9445b5a47/' + data.results[0].geometry.location.lat +',' + data.results[0].geometry.location.lng;
-    
-    $.ajax({                    //calls Forecast for Developers api to get current weather information.
-        dataType: "jsonp",
-        url: url2,
-        success: showOutput,      // calls showOutput to display current weather for specified city name.
-        error: function() {
-            $("#message").text("Forecast for Developers website got busy processing your request. Please try again later.");
-        }
-    });
-};
+// the .done deferred Promise callback - this could be cleaned up with templating
+    showOutput = function(out) {
+        content.text(Date(out.currently.time));
+        content.append("</br> </br> Temperature : " + out.currently.temperature + "F </br>");
+        content.append(" </br> Humidity :  " + out.currently.humidity + "% </br>");
+        content.append(" </br>Icon: " + out.currently.icon + "</br>");
+        content.append(" </br>Wind Speed: " + out.currently.windSpeed + "mph </br>");
+        content.append(" </br>Visibility: " + out.currently.visibility + "m </br>");
+        content.append(" </br>pressure: " + out.currently.pressure + "mb");
+        return message.text("Summary: " + out.daily.summary);
+    },
 
+// The .fail deferred Promise callback
+    showError = function(err) {
+        if (err) console.error(err);
+        return message.text("Forecast for Developers website got busy processing your request. Please try again later.");
+    },
 
-findWeather = function(address) {
-    url1 = 'http://maps.googleapis.com/maps/api/geocode/json';      //Gets longitude and lattitude from Googleapi.
-    var dataparam = {
-        sensor: false,                           //these 2 parameters required to send to Google api to get latitude and longitude.
-        address: address
+//  that url is kind of bulky, let's drop it in a function that just returns the parsed uri
+    parseGeo = function(lat, lon) {
+        return 'https://api.forecast.io/forecast/73ae89cc503818fc307b31e9445b5a47/' + lat + ',' + lon;
     };
-    $.ajax ({                                    //makes ajax call 
-        data: dataparam,
-        dataType: "json",
-        url: url1,
-        success: getForecast,           //calls back (getForecast)Forecast for Developers api to find weather for that lat and long.
-        error: function() {
-             $("#message").text("Enter a proper city name.");
-        }
-        
-    });
-};
 
+// Okay! We're still using basically the same convention - event listeners trigger asynchrounous requests
+searchId.on("keydown", function() {
+
+    // no need to operate on the event, just pass this to jQuery to scope access to jQuery methods such as .val()
+    var address = $(this).val(),
+        checkAddress = address.substr(address.indexOf(",") + 1);
+
+    // Some quick and dirty validation; more resilient and device-agnostic input validation than keycodes
+    if (address.indexOf(",") != -1) {
+        if (checkAddress.replace(" ", "").length == 2) {
+
+            // Set up query data for google api call
+            var dataparam = {
+                sensor: false,
+                address: address
+            };
+
+            // The click listener only attaches if the search input is valid and ready to send
+            buttonId.on("click", function() {
+
+                // Set a token to capture the jqXHR objects so we can chain deferred Promises to it
+                var request = $.ajax({
+                        data: dataparam,
+                        url:'http://maps.googleapis.com/maps/api/geocode/json'
+                    }),
+
+                // Set up handlers for the deferred promise -
+                    chained = request.then(
+
+                        // success - .done
+                        function(data) {
+                            message.empty();
+                            cityname.text("Current weather in " + data.results[0].address_components[0].long_name);
+                            console.log(data);
+
+                            // Return the resolved jqXHR deferred Promise - this will be chained to the next call
+                            return $.ajax({
+                                dataType: "jsonp",
+                                url: parseGeo(data.results[0].geometry.location.lat, data.results[0].geometry.location.lng)
+                            });
+                        },
+
+                        // fail - .fail
+                        function(err) {
+                            return showError(err);
+                        }
+                    );
+                // .then is simply shorthand for .done and .fail, which looks like this when written out longhand -
+                // if the request succeeded, return the success handler
+                chained.done(
+                    function(data) {
+                        return showOutput(data);
+                    })
+                    // .fail - if the request fails for any reason
+                    .fail(
+                    function(err) {
+                        return showError(err);
+                    }
+                );
+            });
+        }
+    }
 });
